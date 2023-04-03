d747d982087f97694f708dfcbc094ddc3c77abe9
Wilber <w.rosales40@gmail.com>
2023-04-02 20:42:22 -0400

    Files

---
+++ main.js
@@ -0,0 +1,32 @@
+function calc(){
+var age = document.getElementById('age');
+var weight = document.getElementById('weight');
+console.log("Your age" + age);
+console.log("Your weight:" + weight);
+}
+
+
+//convert weight lbs to kg
+//weightKG = (weightLbs * 0.453592)
+//convert height to cm
+//convert feet to inches and add to inches
+//heightCM = ( * 2.54)
+
+
+//function bmrCalc() {
+    // if female:
+    // bmr = 10 * weightKG + 6.25 * heightCM - 5 * (double)age - 161;
+    // if male:
+    // bmr = 10 * weightKG + 6.25 * heightCM - 5 * (double)age + 5;
+    
+//}
+
+function tdeeCalc(){
+    
+}
+
+//function printStats(){
+    //Your TDEE:
+
+    //Your stats:
+//}
